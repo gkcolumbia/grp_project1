8acb2df725c5fc489cedf62ba892d010a22c3f61
gabriela-pinto <44420564+gabriela-pinto@users.noreply.github.com>
2018-12-02 01:57:08 -0500

    Update psql_create_tables.py

--- psql_create_tables.py
+++ psql_create_tables.py
@@ -21,9 +21,9 @@ name = "property"
 columns = "id SERIAL PRIMARY KEY, address TEXT, year_built INT, num_of_bedrooms INT, num_of_bathrooms INT, sqft FLOAT, last_year_sold INT, last_price_sold FLOAT" 
 create_table(name, columns)
 
-nombre = "plaintiff"
+name2 = "plaintiff"
 columns_pl = "id SERIAL PRIMARY KEY, name VARCHAR"
-create_table(nombre, columns_pl)
+create_table(name2, columns_pl)
 
 table2 = "foreclosure"
 columns_table2 = "sheriff_id TEXT PRIMARY KEY, property_id INT, initial_status VARCHAR, initial_status_date DATE, last_status VARCHAR, last_status_date DATE, plaintiff_id INT, defendant TEXT, default_amount FLOAT, sale_date DATE, FOREIGN KEY(property_id) REFERENCES property(id), FOREIGN KEY(plaintiff_id) REFERENCES plaintiff(id)"
